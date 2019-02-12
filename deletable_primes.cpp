36cfc7ebd44673ab61657e62650e8f77bae6b47f
MoritzEichhorn <moritz.eichhorn@outlook.com>
2019-02-12 13:25:43 +0100

    Finished

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -10,3 +10,43 @@
  * Implementation of deletable_primes.h.
  * ----------------------------------------------------------
  */
+ #include "deletable_primes.h"
+ #include <math.h>
+ unsigned long remove_digit(int index, unsigned long n){
+   int behind = 0;
+
+   if(index > get_length(n)){
+     return n;
+   }
+   behind = n % (int)pow(10, index);
+   n /= pow(10,index+1);
+   return n*pow(10, index)+behind;
+ }
+
+ int get_ways(unsigned long p){
+   if(!is_prime(p)){return 0;}
+   if(p<10){return 1;}
+   int count = 0;
+   for(int i = 0; i < get_length(p); i++){
+     count += get_ways(remove_digit(i, p));
+   }
+   return count;
+ }
+
+ int get_length(unsigned long n){
+   if(n == 0){
+     return 1;
+   }
+   return floor(log10(n))+1;
+ }
+ bool is_prime(unsigned long n){
+   if(n <= 1){
+     return false;
+   }
+   for(int i = 2; i <= sqrt(n); i++){
+    if(n%i == 0){
+      return false;
+    }
+   }
+   return true;
+ }
